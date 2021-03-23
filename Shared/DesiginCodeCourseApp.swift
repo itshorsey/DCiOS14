9031a6eae56a7b098fe35f2c538962268048d18a
Jonatha Horsman <jonathan.e.horsman@gmail.com>
2021-03-23 16:15:42 -0700

    Initial Commit

---
+++ Shared/DesiginCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesiginCodeCourseApp.swift
+//  Shared
+//
+//  Created by Jonathan Horsman on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DesiginCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
