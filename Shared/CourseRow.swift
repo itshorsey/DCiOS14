f9c22ab5f06613119b61ba6f0cdcccf1086769ed
Jonatha Horsman <jonathan.e.horsman@gmail.com>
2021-04-08 08:57:31 -0700

    first commit

---
+++ Shared/CourseRow.swift
@@ -0,0 +1,20 @@
+//
+//  CourseRow.swift
+//  DesiginCodeCourse
+//
+//  Created by Jonathan Horsman on 3/23/21.
+//
+
+import SwiftUI
+
+struct CourseRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseRow()
+    }
+}
---
+++ Shared/CoursesView.swift
@@ -0,0 +1,20 @@
+//
+//  CoursesView.swift
+//  DesiginCodeCourse
+//
+//  Created by Jonathan Horsman on 3/23/21.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
